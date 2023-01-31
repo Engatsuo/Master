47f1a39090b92883ca2412d1f313730013bec114
Nikola Tešanović <100740409+Engatsuo@users.noreply.github.com>
2023-01-31 19:40:39 +0100

    HF5

--- Creator_HF5.R
+++ Creator_HF5.R
@@ -10,4 +10,4 @@ a <- rnorm(n=40, m=1, sd=1)
 someData <- matrix(a,nrow=20,ncol=2)
 
 h5write(someData, file = "Test_Sample.h5", name="Test_Sample/Test")
-h5dump("Test_Sample.h5")
+h5dump("test_h5.h5")
