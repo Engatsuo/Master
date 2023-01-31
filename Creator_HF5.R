0fc7291843d0efb8396ddd966ed199626c232d89
Nikola Tešanović <100740409+Engatsuo@users.noreply.github.com>
2023-01-31 19:11:41 +0100

    HF5 creator

---
+++ Creator_HF5.R
@@ -0,0 +1,13 @@
+library(rhdf5)
+
+h5createFile("Test_Sample.h5")
+
+h5createGroup("Test_Sample.h5", "Test_Sample")
+
+h5ls("Test_Sample.h5")
+
+a <- rnorm(n=40, m=1, sd=1) 
+someData <- matrix(a,nrow=20,ncol=2)
+
+h5write(someData, file = "Test_Sample.h5", name="Test_Sample/Test")
+h5dump("Test_Sample.h5")
